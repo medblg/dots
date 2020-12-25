de64e87d7971dc0c8997ff5ba44578a7354a21dd
s1m0x <s1m0x@yahoo.com>
2020-12-25 19:38:16 +0100

    automate installation

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# install my necessary progs
+
+sudo apt install tmux vim redshift python3 python3-setuptools dmenu -y
+cp .tmux.conf .vimrc -t ~/
+cp nocaps.desktop ~/.config/autostart/
+
+
+
+#  dmenu_run -b keybinding
