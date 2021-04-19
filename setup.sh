8614bc37c47753709d84da3930193817261efb6e
s1m0x <s1m0x@yahoo.com>
2021-04-19 22:12:18 +0000

    update things

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 # install my necessary progs
 
-sudo apt install tmux vim redshift python3 python3-setuptools dmenu -y
-cp .tmux.conf .vimrc -t ~/
-cp nocaps.desktop ~/.config/autostart/
-
+sudo apt update
+sudo apt install tmux vim terminator redshift python3 python3-setuptools dmenu -y
+ln -s $PWD/.tmux.conf ~/.tmux.conf
+ln -s $PWD/.vimrc ~/.vimrc
+mkdir -p ~/.config/autostart && cp nocaps.desktop ~/.config/autostart/
+cd ~/.config && git clone https://github.com/s1m0x/.config && cd .config
+ln -s $PWD/terminator ~/.config/terminator
 
 
 #  dmenu_run -b keybinding
+
+
